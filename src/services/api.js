e0a3f216688cc6a14baadc38655b58826c578275
Bryan Espejo <u202213278@upc.edu.pe>
2025-06-12 21:58:13 -0500

    changue fake api for live api

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 // src/services/api.js
 
 // Apunta directamente al backend local (JSON Server)
-const API_BASE = 'http://localhost:3000'
+const API_BASE = 'https://fake-api-bsyr.onrender.com/'
 
 export default API_BASE
